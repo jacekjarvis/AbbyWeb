7aa0d88f2479325c9457392c523aac3b9b664826
jacekjarvis <97366756+jacekjarvis@users.noreply.github.com>
2022-02-02 16:08:27 -0400

    Add project files.

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage="The Display Order has to be between 1 and 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+
+    }
+}
